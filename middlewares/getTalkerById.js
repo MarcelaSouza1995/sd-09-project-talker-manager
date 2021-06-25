5dee3d9d40fe5bf736bd0caa7042d5b72d774efe
giovani.diccini <giovani-diccini@pop-os.localdomain>
2021-06-25 17:34:25 -0300

    fix req 2

--- middlewares/getTalkerById.js
+++ middlewares/getTalkerById.js
@@ -4,10 +4,8 @@ const rescue = require('express-rescue');
 const getTalkerById = rescue(async (req, res) => {
   const buffTalkers = await fs.readFile('./talker.json');
   const talkers = JSON.parse(buffTalkers.toString('utf-8'));
-  console.log(talkers);
   const { id } = req.params;
   const talker = talkers.find((curr) => curr.id === Number(id));
-  console.log(talker);
   if (talker) {
     return res.status(200).json(talker);
   }
