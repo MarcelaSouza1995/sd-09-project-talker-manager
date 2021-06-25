7bb3accf11e28e3956552ee9768b84718a828a25
Rafael Reis <rafaelrnascimento2006@gmail.com>
2021-06-25 19:16:22 -0300

    requisito 2 feito

---
+++ middlewares/getTalkerById.js
@@ -0,0 +1,10 @@
+const getData = require('../services/getData');
+
+const getTalkerById = async (req, res) => {
+  const { id } = req.params;
+  const talker = await getData().then((arr) => arr.find((e) => e.id === +id));
+  if (!talker) res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  res.status(200).json(talker);
+};
+
+module.exports = getTalkerById;
--- middlewares/index.js
+++ middlewares/index.js
@@ -1,9 +1,9 @@
 const getAllTalkers = require('./getAllTalkers');
-// const getTalkerById = require('./getTalkerById');
+const getTalkerById = require('./getTalkerById');
 // const login = require('./login');
 // const createTalker = require('./createTalker');
 // const editTalker = require('./editTalker');
 // const deleteTalker = require('./deleteTalker');
 // const searchTalker = require('./searchTalker');
 
-module.exports = { getAllTalkers };
+module.exports = { getAllTalkers, getTalkerById };
