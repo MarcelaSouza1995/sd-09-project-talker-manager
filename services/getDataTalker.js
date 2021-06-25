e32ba683e4fc20025915857a5b962955c50507ba
Diogenes <diogenesfelipe.s@outlook.com>
2021-06-25 15:07:25 -0300

    fix/fs promises

--- services/getDataTalker.js
+++ services/getDataTalker.js
@@ -1,9 +1,7 @@
-const fs = require('fs/promises');
+const fs = require('fs').promises;
 
 const getTalkers = async () => JSON.parse(
   await fs.readFile('./talker.json', 'utf-8'),
 );
-
-getTalkers().then((item) => console.log(item));
 
 module.exports = getTalkers;
