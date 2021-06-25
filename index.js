a483f5d75dec6089cbbbe4d08d50b151a9aa45eb
BBrolesi <brunohsbrolesi@gmail.com>
2021-06-25 18:59:25 -0300

    feat: implements login endpoint

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require('express');
+const crypto = require('crypto');
 const bodyParser = require('body-parser');
 const { getTalkers, findTalkerById } = require('./services/talker');
+const { validateFields } = require('./services/login');
 
 const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
 const HTTP_NOT_FOUND_STATUS = 404;
+const HTTP_BAD_REQUEST_STATUS = 400;
 const PORT = '3000';
 
 // não remova esse endpoint, e para o avaliador funcionar
@@ -36,5 +39,16 @@ app.get('/talker/:id', async (request, response) => {
     response.status(HTTP_OK_STATUS).json(talker);
   } catch (err) {
     response.status(HTTP_NOT_FOUND_STATUS).json({ message: err.message });
+  }
+});
+
+app.post('/login', (request, response) => {
+  const { email, password } = request.body;
+  try {
+    validateFields(email, password);
+    const token = crypto.randomBytes(8).toString('hex');
+    response.status(HTTP_OK_STATUS).json({ token });
+  } catch (err) {
+    response.status(HTTP_BAD_REQUEST_STATUS).json({ message: err.message });
   }
 });
