918d6bb643d0ce633b8f65897ba31f44945b1cd3
Renato Feitosa <pythaqua@gmail.com>
2021-06-25 19:37:00 -0300

    feat: Add 1st requirement

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const data = require('./talker.json');
 
 const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
+
+app.get('/talker', (req, res) => {
+  res.status(200).send(data);
+});
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
