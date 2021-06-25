0a441ca989be128b3bcc8a265f3637bf25cf644f
brenno-calado <brenno.calado@gmail.com>
2021-06-25 19:23:15 -0300

    getTalkers for every endpoint

--- index.js
+++ index.js
@@ -8,12 +8,14 @@ app.use(bodyParser.json());
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
-const getTalkers = async (request, response, next) => {
+const getTalkers = async (request, _response, next) => {
   let talkersList = await fs.readFile('./talker.json', 'utf8');
   talkersList = JSON.parse(talkersList);
   request.talkersList = talkersList;
   next();
 };
+
+app.use(getTalkers);
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
@@ -21,9 +23,8 @@ app.get('/', (_request, response) => {
 });
 
 // Retorna todos os palestrantes. Caso não tenha palestrantes, um array vazio é retornado
-app.get('/talker', getTalkers, (request, response) => {
+app.get('/talker', (request, response) => {
   const { talkersList } = request;
-  console.log(talkersList);
   response.status(HTTP_OK_STATUS).json(talkersList);
 });
 
