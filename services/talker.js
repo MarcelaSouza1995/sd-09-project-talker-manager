3bfc386914a7f44d36b42bba238c7fadaa12b2e1
BBrolesi <brunohsbrolesi@gmail.com>
2021-06-25 17:21:12 -0300

    refactor: getTalkers

--- services/talker.js
+++ services/talker.js
@@ -1,11 +1,16 @@
 const fs = require('fs').promises;
 
-const getTalkers = async () => {
+const readTalkersFile = async () => {
     const talkers = await fs.readFile('./talker.json', 'utf-8', (err, data) => {
         if (err) throw err;
         return data;
       });
     return JSON.parse(talkers);
+};
+
+const getTalkers = async () => {
+    const talkers = await readTalkersFile();
+    return talkers;
 };
 
 module.exports = {
