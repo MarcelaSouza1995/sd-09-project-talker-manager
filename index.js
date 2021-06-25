58b3730b55cc38c47f63619bdde1abe87c981000
Vagner Pereira <sousa.vagner82@gmail.com>
2021-06-25 16:31:06 -0300

    requisito 2

--- index.js
+++ index.js
@@ -12,9 +12,17 @@ const PORT = '3000';
 
 // requisito 1
 app.get('/talker', rescue(async (req, res) => {
-  const talker = await talkerFunc.readTalker();
-  if (!talker) return res.status(HTTP_OK_STATUS).json([]);
-  return res.status(HTTP_OK_STATUS).json(talker);
+  const talkers = await talkerFunc.readTalker();
+  if (!talkers) return res.status(HTTP_OK_STATUS).json([]);
+  return res.status(HTTP_OK_STATUS).json(talkers);
+}));
+// requisito 2
+app.get('/talker/:id', rescue(async (req, res) => {
+  const talkers = await talkerFunc.readTalker();
+  const { id } = req.params;
+  const talkerById = talkers.find((talker) => talker.id === id);
+  if (!talkerById) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  return res.status(200).json(talkerById);
 }));
 
 // não remova esse endpoint, e para o avaliador funcionar
