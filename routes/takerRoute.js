48d7268b758d6f122e0d05d802c3c0bf4299b4ec
tiago-yoneda <xyonedax@gmail.com>
2021-06-25 15:09:52 -0300

    requisito 2 - completed

--- routes/takerRoute.js
+++ routes/takerRoute.js
@@ -7,11 +7,40 @@ const router = express.Router();
 // O endpoint deve retornar um array com todas as pessoas palestrantes cadastradas. Devendo retornar o status 200.
 
 router.get('/', async (_req, res) => {
-  const data = await fs.readFile('./talker.json', 'utf8').then((response) => JSON.parse(response));
+  const talkers = await fs.readFile('./talker.json', 'utf8')
+    .then((response) => JSON.parse(response));
 
   res.status(200).json(
-    data,
+    talkers,
   );
 });
+
+// Caso não seja encontrada uma pessoa palestrante com base no id da rota, o endpoint deve retornar o status 404.
+router.route('/:id')
+  .all(async (req, res, next) => {
+    const myId = Number(req.params.id);
+
+    const talkers = await fs.readFile('./talker.json', 'utf8')
+    .then((response) => JSON.parse(response));
+
+  const myTalker = talkers.find(({ id }) => id === myId);
+
+  if (!myTalker) {
+    return res.status(404).json({
+      message: 'Pessoa palestrante não encontrada',
+    });
+  }
+
+  req.myTalker = myTalker;
+
+  next();
+  })
+  // 2 - Crie o endpoint GET /talker/:id
+  // O endpoint deve retornar uma pessoa palestrante com base no id da rota. Devendo retornar o status 200 ao fazer uma requisição /talker/1.
+  .get((req, res) => {
+    res.status(200).json(
+      req.myTalker,
+    );
+  });
 
 module.exports = router;
