7e9ec9ca3060f8a4f57e52fa7e573d305edd6f1b
giovani.diccini <giovani-diccini@pop-os.localdomain>
2021-06-25 14:53:55 -0300

    fix req 1

--- middlewares/getAllTalkers.js
+++ middlewares/getAllTalkers.js
@@ -1,7 +1,7 @@
 const talkers = require('../talker.json');
 
 const getAllTalkers = (req, res) => {
-  if (talkers.length) {
+  if (talkers.length > 0) {
     return res.status(200).json(talkers);
   }
   return res.status(200).json([]);
