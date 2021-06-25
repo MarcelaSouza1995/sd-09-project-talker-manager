ce55a0d22c5c374559b281f8b4a86500f18bf107
brenno-calado <brenno.calado@gmail.com>
2021-06-25 18:15:16 -0300

    endpoint talker

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs').promises;
 
 const app = express();
 app.use(bodyParser.json());
@@ -7,10 +8,18 @@ app.use(bodyParser.json());
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
+const getTalkers = async (_request, response, _next) => {
+  const talkersList = await fs.readFile('./talker.json', 'utf8');
+  response.status(HTTP_OK_STATUS).json({ talkersList: JSON.parse(talkersList) });
+};
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
+
+// Este endpoint retorna todos os palestrantes. Caso não tenha palestrantes, um array vazio é retornado
+app.get('/talker', getTalkers);
 
 app.listen(PORT, () => {
   console.log('Online');
