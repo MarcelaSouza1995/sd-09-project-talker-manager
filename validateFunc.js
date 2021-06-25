d3bd231ecdd12a560ef4ba5488d151d0baf635e7
Vagner Pereira <sousa.vagner82@gmail.com>
2021-06-25 18:31:21 -0300

    requisito 3

--- validateFunc.js
+++ validateFunc.js
@@ -2,10 +2,10 @@ const validationEmail = (req, res, next) => {
     const { email } = req.body;
     const re = /\S+@\S+\.\S+/;
     if (!email || email.length === 0) {
-        return res.status(401).json({ message: 'O campo "email" é obrigatório' });
+        return res.status(400).json({ message: 'O campo "email" é obrigatório' });
     }
     if (!re.test(email)) {
-        return res.status(401).json({ message: 'O "email" deve ter o formato "email@email.com"' });
+        return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
     }
     return next();
 };
@@ -13,10 +13,10 @@ const validationEmail = (req, res, next) => {
 const validationPassword = (req, res, next) => {
     const { password } = req.body;
     if (!password || password.length === 0) {
-        return res.status(401).json({ message: 'O campo "password" é obrigatório' });
+        return res.status(400).json({ message: 'O campo "password" é obrigatório' });
     }
     if (password.length < 5) {
-        return res.status(401).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+        return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
     }
     return next();
 };
