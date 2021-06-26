3a63e5f9f6a50ff578225dc0992eb59efe78f0a5
ThamirysSantos <thamirysgoncalves.prog@gmail.com>
2021-06-26 17:19:21 -0300

    Fix login messages requisito 03

--- middlewares/loginValidator.js
+++ middlewares/loginValidator.js
@@ -3,13 +3,13 @@ const { emailValidator, passwordValidator } = require('../util/validationFunctio
 const loginValidator = (req, res, next) => {
   const { email, password } = req.body;
 
-  if (!email) res.status(400).json({ message: 'O campo "email" é obrigatório' });
-  if (!password) res.status(400).json({ message: 'O campo "password" é obrigatório' });
+  if (!email) res.status(400).json({ message: 'O campo \"email"\ é obrigatório' });
+  if (!password) res.status(400).json({ message: 'O campo \"password"\ é obrigatório' });
   if (!emailValidator(email)) { 
-    return res.status(400).json({ message: 'O "email" deve ter o formato \'email@email.com\'' });
+    return res.status(400).json({ message: 'O \"email"\ deve ter o formato \"email@email.com\"' });
   }
   if (!passwordValidator(password)) {
-    return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+    return res.status(400).json({ message: 'O \"password"\ deve ter pelo menos 6 caracteres' });
   }
   next();
 };
