061207af6311c5465ea9b73b8302612bec18f9c6
Rafael Pufal <rafael.pufal23@gmail.com>
2021-06-26 15:15:04 -0300

    correçoes ao req 2

--- middlewares/talkerId.js
+++ middlewares/talkerId.js
@@ -4,11 +4,12 @@ const talkerId = async (req, res, next) => {
   const { id } = req.params;
   try {
     const file = await fs.readFile('./talker.json');
-    const talkers = JSON.parse(file);
+    const talkers = await JSON.parse(file);
     const idInfo = talkers.find((currFile) => currFile.id === parseInt(id, 10));
-    return res.status(200).send(JSON.stringify(idInfo));
-  } catch (error) {
+    if (idInfo) return res.status(200).json(idInfo);
     return next({ code: 404, message: 'Pessoa palestrante não encontrada' });
+  } catch (error) {
+    console.log(error);
   }
 };
 
