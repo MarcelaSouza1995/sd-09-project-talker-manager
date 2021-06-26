0976b03df04aeac50700aa672466926954e6e770
Nathália <nathaliaveneziano@icloud.com>
2021-06-26 18:25:44 -0300

    FEAT - Requisito 1

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs');
 
 const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
+
+// Requisito 01
+app.get('/talker', (req, res) => {
+  const talkersData = JSON.parse(fs.readFileSync('talker.json', 'utf8'));
+  res.status(HTTP_OK_STATUS).json(talkersData);
+});
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
