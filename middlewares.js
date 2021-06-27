ae2d516192bd2831a7d35131d1541ae6f5d4d992
Ian Zwanck Goodwin <ianzwanck@yahoo.com.br>
2021-06-26 23:48:06 -0300

    req1/talker.promises.json.parse

--- middlewares.js
+++ middlewares.js
@@ -1,8 +1,12 @@
 const fs = require('fs').promises;
 
 const getTalkers = async (_req, res) => {
-  const talkers = await fs.readFile('./talker.json', 'utf-8');
-  res.status(200).json(talkers);
+  let talkers = await fs.readFile('./talker.json', 'utf-8');
+  //talkers = talkers.substring(1);
+  //const last = talkers.length - 2;
+  //talkers = talkers.substring(1, last);
+  JSON.parse(talkers);
+  res.status(200).send(talkers);
 };
 
 module.exports = { getTalkers };
