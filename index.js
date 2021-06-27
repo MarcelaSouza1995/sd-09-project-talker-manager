a70730da492b2107baebec244d704d32ef411e96
Nathália <nathaliaveneziano@icloud.com>
2021-06-26 22:17:50 -0300

    FEAT - Requisito 3

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 
 const getAllTalkers = require('./middlewares/requisito01');
 const getTalkerById = require('./middlewares/requisito02');
+const loginValidate = require('./middlewares/requisito03');
 
 const app = express();
 app.use(bodyParser.json());
@@ -13,10 +14,23 @@ const PORT = '3000';
 // Requisitos
 app.get('/talker', getAllTalkers);
 app.get('/talker/:id', getTalkerById);
+app.post('/login', loginValidate);
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+// Erro
+app.use((err, req, res, next) => {
+  console.log(err);
+  if (err.code && err.status) {
+    return res
+      .status(err.status)
+      .json({ message: err.message });
+  }
+
+  return res.status(500).json({ message: err.message });
 });
 
 app.listen(PORT, () => {
