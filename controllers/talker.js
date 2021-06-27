b1c5cfdc6f537a5e7b468165fb39f8dc8d38b49a
genadireuben <genadireuben91@gmail.com>
2021-06-27 19:39:57 -0300

    implementa endpoint search

--- controllers/talker.js
+++ controllers/talker.js
@@ -24,6 +24,26 @@ const verifyRate = require('../middlewares/verifyRate');
 
 talkerRouter.get('/', readFileMiddleware);
 
+talkerRouter.get(
+  '/search',
+  verifyToken,
+  async (req, res) => {
+  const { q } = req.query;
+  const data = await talkerFile();
+  const filteredTalkers = data.filter((talker) => talker.name.includes(q));
+
+  if (!q || q.length === 0) {
+    return res.status(200).json(data);
+  }
+
+  if (filteredTalkers.length === 0) {
+    return res.json(200).json([]);
+  }
+
+  return res.status(200).json(filteredTalkers);
+  },
+);
+
 talkerRouter.get('/:id', findTalker);
 
 talkerRouter.post(
