3ba958a86d5c48a0991b45b386ec584a932ae5c2
genadireuben <genadireuben91@gmail.com>
2021-06-27 19:02:01 -0300

    implementa edit e delete de palestrante

--- controllers/talker.js
+++ controllers/talker.js
@@ -10,8 +10,6 @@ const readFileMiddleware = require('../middlewares/readFileMiddleware');
 
 const findTalker = require('../middlewares/findTalker');
 
-// const registerTalker = require('../middlewares/registerTalker');
-
 const verifyToken = require('../middlewares/verifyToken');
 
 const verifyTalkerName = require('../middlewares/verifyTalkerName');
@@ -48,6 +46,45 @@ talkerRouter.post(
 
     return res.status(201).json({ ...newTalker });
   },
+);
+
+talkerRouter.put(
+  '/:id',
+  verifyToken,
+  verifyTalkerName,
+  verifyTalkerAge,
+  verifyTalk,
+  verifyWatchedAt,
+  verifyRate,
+  async (req, res) => {
+    const data = await talkerFile();
+    const { id } = req.params;
+    const filteredTalker = data.filter((talker) => talker.id !== +(id));
+
+    const editedTalker = {
+      id: Number(id),
+       ...req.body,
+    };
+    filteredTalker.push(editedTalker);
+    console.log(filteredTalker);
+
+    await fs.writeFile('./talker.json', JSON.stringify(filteredTalker, null, 2));
+
+    return res.status(200).json(editedTalker);
+  },
+);
+
+talkerRouter.delete(
+  '/:id',
+  verifyToken,
+async (req, res) => {
+  const data = await talkerFile();
+  const { id } = req.params;
+  const filteredTalkers = data.filter((talker) => talker.id !== +(id));
+
+  await fs.writeFile('./talker.json', JSON.stringify(filteredTalkers, null, 2));
+  return res.status(200).json({ message: 'Pessoa palestrante deletada com sucesso' });
+},
 );
 
 module.exports = talkerRouter;
