6221b8b9a06dfa3ead800d673cec0e985afc58a3
Felipe Pimentel <lipe.pimentel89@gmail.com>
2021-06-28 18:29:57 -0300

    fix req 1 & 2

--- middlewares/talkers.js
+++ middlewares/talkers.js
@@ -6,13 +6,13 @@ const talkersMiddleware = async (req, res) => {
     .readFile('talker.json', 'utf-8')
     .then((fileContent) => JSON.parse(fileContent));
   if (!id) {
-    res.status(200).json(talkers);
+    return res.status(200).json(talkers);
   }
   const talkerById = talkers.filter((talker) => talker.id === Number(id));
   if (talkerById.length > 0) {
-    res.status(200).json(...talkerById);
+    return res.status(200).json(...talkerById);
   }
-  res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
 };
 
 module.exports = talkersMiddleware;
