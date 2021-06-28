1fa626fb8fc4e70473ce246b9c886f9a03de77bb
J.Henrique <jhenriqueribeirodesa@gmail.com>
2021-06-28 18:13:29 -0300

    Add insert method to talkerRepository

--- src/talkerRepository.js
+++ src/talkerRepository.js
@@ -9,4 +9,12 @@ function findById(id) {
     return findAll().find((talker) => talker.id === +id);
 }
 
-module.exports = { findAll, findById };
+function insert({ name, age, talk: { rate, watchedAt } }) {
+    const talkers = findAll();
+    const talker = { id: talkers.length + 1, name, age, talk: { rate, watchedAt } };
+    talkers.push(talker);
+    fs.writeFileSync('./talker.json', JSON.stringify(talkers), { encoding: 'utf8' });
+    return talker;
+}
+
+module.exports = { findAll, findById, insert };
