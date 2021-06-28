6db73ada123d58379ea7ef243884a74c7ca98a4d
Leticia Lima <letylima96@gmail.com>
2021-06-28 17:12:18 -0300

    requisito 3

--- index.js
+++ index.js
@@ -27,6 +27,24 @@ app.get('/talker/:id', (request, response) => {
   return response.status(404).json({ message: 'Pessoa palestrante não encontrada' });
 });
 
+app.post('/login', (request, response) => {
+  const { email, password } = request.body;
+  const regexEmail = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+  if (!email) {
+    return response.status(400).json({ message: 'O campo "email" é obrigatório' });
+  }
+  if (!regexEmail.test(email)) {
+    return response.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
+  }
+  if (!password) {
+    return response.status(400).json({ message: 'O campo "password" é obrigatório' });
+  }
+  if (password.length < 6) {
+    return response.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+  }
+  return response.status(200).json({ token: '7mqaVRXJSp886CGr' });
+});
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
