6ccbb502f473ca9bd322caa4da2fbb058dc73e0a
Rafael Pufal <rafael.pufal23@gmail.com>
2021-06-28 13:06:36 -0300

    req 5

--- middlewares/putTalker.js
+++ middlewares/putTalker.js
@@ -4,14 +4,15 @@ const putTalker = async (req, res, _next) => {
   const talkers = await fs.readFile('./talker.json')
   .then((result) => JSON.parse(result));
   const { id } = req.params;
+  const idInt = parseInt(id, 10);
   const { name, age, talk } = req.body;
   const talkerRegistered = {
-    id,
+    id: idInt,
     name,
     age,
     talk,
   };
-  talkers.splice(id - 1, id - 1, talkerRegistered);
+  talkers.splice(idInt - 1, idInt - 1, talkerRegistered);
   await fs.writeFile('./talker.json', JSON.stringify(talkers));
   return res.status(200).json(talkerRegistered);
 };
