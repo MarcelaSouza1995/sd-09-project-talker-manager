a93c77b76f44468b3c937c0c6e8e284e83cda181
saulokirchmaier <saulokirchmaier@gmail.com>
2021-06-28 10:35:01 -0300

    validate email e password lint

--- util/validateEmail.js
+++ util/validateEmail.js
@@ -4,7 +4,8 @@ function validateEmail(request, response, next) {
   const { email } = request.body;
 
   if (!email) {
-    return response.status(HTTP_BAD_REQUEST_STATUS).json({ message: 'O campo "email" é obrigatório' });
+    return response.status(HTTP_BAD_REQUEST_STATUS)
+      .json({ message: 'O campo "email" é obrigatório' });
   }
   const validEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
   if (!validEmail) {
--- util/validatePassword.js
+++ util/validatePassword.js
@@ -3,7 +3,8 @@ const HTTP_BAD_REQUEST_STATUS = 400;
 function validatePassword(request, response, next) {
   const { password } = request.body;
   if (!password) {
-    return response.status(HTTP_BAD_REQUEST_STATUS).json({ message: 'O campo "password" é obrigatório' });
+    return response.status(HTTP_BAD_REQUEST_STATUS)
+      .json({ message: 'O campo "password" é obrigatório' });
   }
   if (password.length < 6) {
     return response.status(HTTP_BAD_REQUEST_STATUS)
